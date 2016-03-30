c1b78c964dbb6ba5eea8a11f9b3cabeed1234f16
Michal Mrowiec <michal.mrowiec@simplesoft.pl>
2016-03-30 14:54:22 +0200

    remove web related files that are not longer required

--- scripts/fab_build_bower_npm.sh
+++ scripts/fab_build_bower_npm.sh
@@ -1,11 +1,1 @@
 #!/bin/sh
-
-export HOME="/home/$1"
-rm -fR node_modules
-npm config set cache /home/$1/.npm
-npm install
-
-export XDG_CONFIG_HOME="/home/$1/.config"
-rm -fR nutrimom/static/libs/bower_components
-bower cache clean
-bower install --production --no-color
