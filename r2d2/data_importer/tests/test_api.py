35b41dcf05b3408e47ad20eda9943c3047efaf9b
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-04-01 19:44:19 +0200

    RD-64 [BE] generic data downloader

--- r2d2/data_importer/tests/test_api.py
+++ r2d2/data_importer/tests/test_api.py
@@ -17,22 +17,46 @@ class DataImporterApiTestCase(APIBaseTestCase):
         self.assertIn('ShopifyStore', registered_models)
 
     def test_importer_flow(self):
-        """ checks the whole importer flow """
+        """ checks the whole importer flow
+            - scheduling the idle task only if authorized
+            - changing status after fetching data
+            - rescheduling already completed tasks """
         self._create_user()
         account = ShopifyStore.objects.create(user=self.user, access_token='token', name='name')
-        not_authorized_account = ShopifyStore.objects.create(user=self.user, access_token='token', name='other-name')
+        not_authorized_account = ShopifyStore.objects.create(user=self.user, name='other-name')
 
-        with mock.patch('r2d2.data_importer.tasks.fetch_data_task.apply_async') as mocked_find:
-            mocked_find.return_value = None
+        with mock.patch('r2d2.data_importer.tasks.fetch_data_task.apply_async') as mocked_fetch_data:
+            mocked_fetch_data.return_value = None
 
             # run importer
             DataImporter.run_fetching_data()
 
-            # TODO: mock - count
-
             # check status #1
-            self.assertEqual(ShopifyStore.objects.get(id=account.id).fetch_status, ShopifyStore.FETCH_SUCCESS)
+            self.assertEqual(ShopifyStore.objects.get(id=account.id).fetch_status, ShopifyStore.FETCH_SCHEDULED)
 
             # check status #2
             self.assertEqual(ShopifyStore.objects.get(id=not_authorized_account.id).fetch_status,
                              ShopifyStore.FETCH_IDLE)
+
+        with mock.patch('r2d2.shopify_api.models.ShopifyStore._fetch_data_inner') as mocked_fetch_data_inner:
+            mocked_fetch_data_inner.return_value = None
+
+            account = ShopifyStore.objects.get(id=account.id)
+            account.fetch_data()
+            self.assertEqual(account.fetch_status, ShopifyStore.FETCH_SUCCESS)
+            mocked_fetch_data_inner.assert_any_call()
+
+        with mock.patch('r2d2.shopify_api.models.ShopifyStore._fetch_data_inner') as mocked_fetch_data_inner:
+            mocked_fetch_data_inner.return_value = None
+
+            not_authorized_account = ShopifyStore.objects.get(id=not_authorized_account.id)
+            not_authorized_account.fetch_data()
+            self.assertEqual(not_authorized_account.fetch_status, ShopifyStore.FETCH_IDLE)
+            mocked_fetch_data_inner.assert_not_called()
+
+        # same check - but from different state
+        with mock.patch('r2d2.data_importer.tasks.fetch_data_task.apply_async') as mocked_fetch_data:
+            mocked_fetch_data.return_value = None
+
+            DataImporter.run_fetching_data()
+            self.assertEqual(ShopifyStore.objects.get(id=account.id).fetch_status, ShopifyStore.FETCH_SCHEDULED)
