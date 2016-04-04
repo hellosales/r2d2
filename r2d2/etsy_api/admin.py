3fc56549b7a0a9c73c40f73f5b3e1e8016842a03
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-04-04 17:30:42 +0200

    RD-56 [BE][MongoDB] store etsy transaction data in MongoDB

--- r2d2/etsy_api/admin.py
+++ r2d2/etsy_api/admin.py
@@ -1,12 +1,49 @@
 # -*- coding: utf-8 -*-
 """ etsy models admin """
 from django.contrib import admin
+from django_mongoengine.admin_support.decorators import dynamic_fields_list_display
 
 from r2d2.etsy_api.models import EtsyAccount
+from r2d2.etsy_api.models import ImportedEtsyShop
+from r2d2.etsy_api.models import ImportedEtsyTransaction
+from r2d2.etsy_api.models import ImportedEtsyReceipt
+# from r2d2.etsy_api.models import ImportedEtsyPayment
+# from r2d2.etsy_api.models import ImportedEtsyPaymentAdjustment
+from r2d2.utils.documents import StorageDocumentAdmin
 
 
 class EtsyAccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'is_authorized')
 
 
+@dynamic_fields_list_display('shop_id', 'shop_name', 'user_id')
+class ImportedEtsyShopAdmin(StorageDocumentAdmin):
+    pass
+
+
+@dynamic_fields_list_display('transaction_id', 'seller_user_id', 'title', 'price', 'receipt_id')
+class ImportedEtsyTransactionAdmin(StorageDocumentAdmin):
+    pass
+
+
+@dynamic_fields_list_display('receipt_id', 'last_modified_tsz')
+class ImportedEtsyReceiptAdmin(StorageDocumentAdmin):
+    pass
+
+
+# @dynamic_fields_list_display('payment_id', 'receipt_id')
+# class ImportedEtsyPaymentAdmin(StorageDocumentAdmin):
+#     pass
+
+
+# @dynamic_fields_list_display('payment_adjustment_id', 'payment_id', 'status')
+# class ImportedEtsyPaymentAdjustmentAdmin(StorageDocumentAdmin):
+#     pass
+
+
 admin.site.register(EtsyAccount, EtsyAccountAdmin)
+admin.site.register(ImportedEtsyShop, ImportedEtsyShopAdmin)
+admin.site.register(ImportedEtsyTransaction, ImportedEtsyTransactionAdmin)
+admin.site.register(ImportedEtsyReceipt, ImportedEtsyReceiptAdmin)
+# admin.site.register(ImportedEtsyPayment, ImportedEtsyPaymentAdmin)
+# admin.site.register(ImportedEtsyPaymentAdjustment, ImportedEtsyPaymentAdjustmentAdmin)
--- r2d2/etsy_api/models.py
+++ r2d2/etsy_api/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """ etsy models """
-from constance import config
+from etsy import Etsy
 from etsy.oauth import EtsyEnvProduction
 from etsy.oauth import EtsyOAuthClient
 from oauth2 import Token
@@ -12,6 +12,7 @@ from django.utils import timezone
 
 from r2d2.data_importer.api import DataImporter
 from r2d2.data_importer.models import AbstractDataProvider
+from r2d2.utils.documents import StorageDynamicDocument
 
 
 class EtsyAccount(AbstractDataProvider):
@@ -20,10 +21,11 @@ class EtsyAccount(AbstractDataProvider):
         this model keeps also token if the user authorized
         our app to use this account"""
     request_token = models.CharField(max_length=255, null=True, blank=True)
+    MAX_REQUEST_LIMIT = 100
 
     @property
     def authorization_url(self):
-        """ getting authorization url for the account & store request token """
+        """ getting authorization url for the account & storing request token """
         if self.is_authorized:
             return None
 
@@ -50,11 +52,120 @@ class EtsyAccount(AbstractDataProvider):
         self.save()
         return True
 
+    def _prepare_api(self):  # TODO: rest :)
+        if not self._etsy_api and self.access_token:
+            client = EtsyOAuthClient(settings.ETSY_API_KEY, settings.ETSY_API_SECRET, etsy_env=EtsyEnvProduction(),
+                                     token=Token.from_string(self.access_token))
+            self._etsy_api = Etsy(settings.ETSY_API_KEY, etsy_env=EtsyEnvProduction, etsy_oauth_client=client)
+            return True
+        else:
+            return False
+
+    def _fetch_user(self):
+        response = self._etsy_api.getUser(user_id='__SELF__')
+        return response[0]['user_id']
+
+    def _fetch_user_shops(self, user_id):
+        shops_ids = []
+        for shop in self._etsy_api.findAllUserShops(user_id=user_id, limit=100):
+            shops_ids.append(shop['shop_id'])
+            ImportedEtsyShop.objects.filter(shop_id=shop['shop_id']).delete()
+            ImportedEtsyShop.create_from_json(self, shop)
+        return shops_ids
+
+    def _fetch_transactions(self, shop_id):
+        offset = ImportedEtsyTransaction.objects.filter(account_id=self.id, shop_id=shop_id).count()
+        kwargs = {
+            'shop_id': shop_id,
+            'offset': offset,
+            'limit': self.MAX_REQUEST_LIMIT
+        }
+        while True:
+            transactions = self._etsy_api.findAllShopTransactions(**kwargs)
+            for transaction in transactions:
+                ImportedEtsyTransaction.objects.filter(transaction_id=transaction['transaction_id']).delete()
+                ImportedEtsyTransaction.create_from_json(self, transaction)
+
+            if len(transactions) < self.MAX_REQUEST_LIMIT:
+                break
+
+            offset += self.MAX_REQUEST_LIMIT
+
+    def _fetch_receipts(self, shop_id):
+        offset = 0
+        kwargs = {
+            'shop_id': shop_id,
+            'offset': 0,
+            'limit': self.MAX_REQUEST_LIMIT
+        }
+        if 'receipt' in self.last_api_items_dates:
+            kwargs['min_last_modified'] = self.last_api_items_dates['receipt']
+        while True:
+            receipts = self._etsy_api.findAllShopReceipts(**kwargs)
+
+            for receipt in receipts:
+                ImportedEtsyReceipt.objects.filter(receipt_id=receipt['receipt_id']).delete()
+                ImportedEtsyReceipt.create_from_json(self, receipt)
+
+                if ('receipt' not in self.last_api_items_dates or
+                        self.last_api_items_dates['receipt'] < receipt['last_modified_tsz']):
+                    self.last_api_items_dates['receipt'] = receipt['last_modified_tsz']
+                    self.save()
+
+            if len(receipts) < self.MAX_REQUEST_LIMIT:
+                break
+
+            kwargs['offset'] += self.MAX_REQUEST_LIMIT
+
     def _fetch_data_inner(self):
-        pass  # TODO
+        if not self._prepare_api():
+            raise Exception('failed to set up API')
+
+        user_id = self._fetch_user()
+        shops_ids = self._fetch_user_shops(user_id)
+        for shop_id in shops_ids:
+            self._fetch_transactions(shop_id)
+            dummy_receipt_ids = self._fetch_receipts(shop_id)
+            # ## it is reduntant, but if Matt decide he wants us to import it:
+            # for receipt_id in recepit_ids:
+            #     # self._etsy_api.findShopPaymentByReceipt(shop_id=, receipt_id=)
+            #     payment_id = self._fetch_payment(shop_id, receipt_id)
+            #     self._fetch_payment_adjustment(payment_id)  # self._etsy_api.findPaymentAdjustments(payment_id=)
+
+    def __init__(self, *args, **kwargs):
+        super(EtsyAccount, self).__init__(*args, **kwargs)
+        self._etsy_api = None
 
     def __unicode__(self):
         return self.name
 
 
 DataImporter.register(EtsyAccount)
+
+
+ETSY_PREFIX = ""  # not necessary, etsy ids are always prefixed, i.e. 'shop_id' or 'user_id', never just 'id'
+
+
+class ImportedEtsyShop(StorageDynamicDocument):
+    account_model = EtsyAccount
+    prefix = ETSY_PREFIX
+
+
+class ImportedEtsyTransaction(StorageDynamicDocument):
+    account_model = EtsyAccount
+    prefix = ETSY_PREFIX
+
+
+class ImportedEtsyReceipt(StorageDynamicDocument):
+    account_model = EtsyAccount
+    prefix = ETSY_PREFIX
+
+
+# class ImportedEtsyPayment(StorageDynamicDocument):
+#     account_model = EtsyAccount
+#     prefix = ETSY_PREFIX
+
+
+# class ImportedEtsyPaymentAdjustment(StorageDynamicDocument):
+#     account_model = EtsyAccount
+#     prefix = ETSY_PREFIX
