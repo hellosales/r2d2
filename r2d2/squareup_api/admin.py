41f645b1c59925598f335c5203ea6b9e363c1da7
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-04-06 20:19:31 +0200

    RD-69 Make proper fields read only in admin

--- r2d2/squareup_api/admin.py
+++ r2d2/squareup_api/admin.py
@@ -12,7 +12,7 @@ from r2d2.utils.documents import StorageDocumentAdmin
 class SquareupAccountAdmin(DataImporterAdmin):
     def get_readonly_fields(self, request, obj=None):
         readonly_fields = super(SquareupAccountAdmin, self).get_readonly_fields(request, obj)
-        return readonly_fields + ('in_authorization',)
+        return readonly_fields + ('in_authorization', 'merchant_id', 'token_expiration')
 
 
 @dynamic_fields_list_display('squareup_id', 'created_at')
