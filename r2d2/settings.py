2e5cd7b09b261539da5e8847812b587a6b9bb819
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-05-18 16:35:54 +0200

    RD-126 Status isn't refreshed after authorizing Etsy account

--- r2d2/settings.py
+++ r2d2/settings.py
@@ -318,7 +318,8 @@ USE_THOUSAND_SEPARATOR = True
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
     }
 }
 
