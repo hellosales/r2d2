88d226ff239bcfa3ea3d2e5ca747585f06c940c8
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-06-09 11:19:16 +0200

    RD-255 [BLOCKER] There is no template in e-mails.

--- r2d2/insights/signals.py
+++ r2d2/insights/signals.py
@@ -8,13 +8,14 @@ data_fetched = Signal(providing_args=["account", "fetched_from_all", "success"])
 
 
 def insight_post_save(sender, instance, created, **kwargs):
-    send_notification = True
-    if instance.generator_class != "Manual":
-        generator_class = getattr(generators, instance.generator_class, None)
-        if generator_class:
-            send_notification = generator_class.EMAIL_NOTIFICATION
-        else:
-            send_notification = False
+    send_notification = created
+    if send_notification:
+        if instance.generator_class != "Manual":
+            generator_class = getattr(generators, instance.generator_class, None)
+            if generator_class:
+                send_notification = generator_class.EMAIL_NOTIFICATION
+            else:
+                send_notification = False
 
     if send_notification:
         # it goes through task since on the post_save we don't have attachments yet
