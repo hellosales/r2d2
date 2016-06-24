8e23000c06aa6156889777415d90a7a8812f0c6c
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-06-24 11:27:28 +0200

    RD-382 [BE] pagination refactor

--- r2d2/insights/api.py
+++ r2d2/insights/api.py
@@ -2,12 +2,12 @@
 """ insights API """
 from rest_framework.generics import GenericAPIView
 from rest_framework.generics import ListAPIView
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from r2d2.insights.models import Insight
 from r2d2.insights.serializers import HeaderDataSerializer
 from r2d2.insights.serializers import InsightSerializer
+from r2d2.utils.pagination import BetterLimitOffsetPagination
 from r2d2.utils.rest_api_helpers import UserFilteredMixin
 
 
@@ -18,7 +18,7 @@ class InsightsListAPI(UserFilteredMixin, ListAPIView):
     serializer_class = InsightSerializer
     queryset = Insight.objects.order_by('-id').all()
     ordering = ('name',)
-    pagination_class = LimitOffsetPagination
+    pagination_class = BetterLimitOffsetPagination
 
 
 class HeaderDataApi(GenericAPIView):
--- r2d2/utils/pagination.py
+++ r2d2/utils/pagination.py
@@ -1,7 +1,69 @@
-# -*- coding: utf-8 -*-
-from rest_framework.pagination import CursorPagination
+from collections import OrderedDict
+
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import replace_query_param
+from rest_framework.response import Response
 
 
-class CursorByIDPagination(CursorPagination):
-    page_size = 20
-    ordering = '-id'
+class IncorrectLimitOffsetError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Malformed request.')
+
+
+class BetterLimitOffsetPagination(LimitOffsetPagination):
+    """
+    A limit/offset based style. For example:
+
+    http://api.example.org/accounts/?limit=100
+    http://api.example.org/accounts/?offset=400&limit=100
+
+    Improvements:
+        - no count,
+        - works
+
+    Warning:
+        - html is not handled
+    """
+    def paginate_queryset(self, queryset, request, view=None):
+        self.limit = self.get_limit(request)
+        if self.limit is None:
+            raise IncorrectLimitOffsetError
+
+        self.offset = self.get_offset(request)
+        if self.offset < 0:
+            self.limit += self.offset  # + because offset is negative
+            self.offset = 0
+        if self.limit <= 0:
+            raise IncorrectLimitOffsetError
+
+        self.request = request
+        self.results = list(queryset[self.offset:self.offset + self.limit])
+        return self.results
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('results', data)
+        ]))
+        return self.default_limit
+
+    def get_offset(self, request):
+        try:
+            return int(request.query_params[self.offset_query_param])
+        except (KeyError, ValueError):
+            return 0
+
+    def get_next_link(self):
+        if len(self.results) < self.limit:
+            return None
+
+        url = self.request.build_absolute_uri()
+        url = replace_query_param(url, self.limit_query_param, self.limit)
+
+        offset = self.offset + self.limit
+        return replace_query_param(url, self.offset_query_param, offset)
