1b5bb58f91ba927ae9f8fac8935ec95e642c76bf
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-07-28 15:42:17 +0200

    test

--- r2d2/insights/api.py
+++ r2d2/insights/api.py
@@ -36,8 +36,6 @@ class HeaderDataApi(GenericAPIView):
 
 class DownloadAttachmentApi(GenericAPIView):
     """ API for getting insights attachments """
-    serializer_class = HeaderDataSerializer
-
     def get(self, request, pk):
         try:
             attachment = InsightAttachment.objects.get(insight__user=request.user, pk=pk)
@@ -46,4 +44,4 @@ class DownloadAttachmentApi(GenericAPIView):
             response["Content-Disposition"] = "attachment; filename=%s" % attachment.file_name
             return response
         except InsightAttachment.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(data={'user': unicode(request.user), 'pk': pk}, status=status.HTTP_404_NOT_FOUND)
