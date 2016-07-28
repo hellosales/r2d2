565b64b97053362385149c9a0db18a5050906bc3
Paweł Krzyżaniak <pawel.krzyzaniak@gmail.com>
2016-07-28 10:59:03 +0200

    RD-419 Secure attachment download from insights email

--- r2d2/insights/api.py
+++ r2d2/insights/api.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 """ insights API """
+import urllib2
+
+from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
 from r2d2.insights.models import Insight
+from r2d2.insights.models import InsightAttachment
 from r2d2.insights.serializers import HeaderDataSerializer
 from r2d2.insights.serializers import InsightSerializer
 from r2d2.utils.pagination import BetterLimitOffsetPagination
@@ -27,3 +32,18 @@ class HeaderDataApi(GenericAPIView):
 
     def get(self, request):
         return Response(self.serializer_class(request.user).data)
+
+
+class DownloadAttachmentApi(GenericAPIView):
+    """ API for getting insights attachments """
+    serializer_class = HeaderDataSerializer
+
+    def get(self, request, pk):
+        try:
+            attachment = InsightAttachment.objects.get(insight__user=request.user, pk=pk)
+            opener = urllib2.urlopen(attachment.file.url)
+            response = HttpResponse(opener.read(), content_type=attachment.content_type)
+            response["Content-Disposition"] = "attachment; filename=%s" % attachment.file_name
+            return response
+        except InsightAttachment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
--- r2d2/insights/api_urls.py
+++ r2d2/insights/api_urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
 
+from r2d2.insights.api import DownloadAttachmentApi
 from r2d2.insights.api import HeaderDataApi
 from r2d2.insights.api import InsightsListAPI
 
@@ -11,4 +12,5 @@ urlpatterns = patterns(
     '',
     url(r'^insights$', InsightsListAPI.as_view(), name='insights'),
     url(r'^header-data$', HeaderDataApi.as_view(), name='header_data_api'),
+    url(r'^insights/download/(?P<pk>[\d]+)$', DownloadAttachmentApi.as_view(), name='insight_download_attachment'),
 )
