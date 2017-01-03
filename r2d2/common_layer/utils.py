9116f3e36a1ef3093ce86095e485a104d99f2644
Matt Laszuk <gen@freshblend.com>
2017-01-03 17:21:38 -0500

    util function for calculating nearest date or number to options in a passed list

--- r2d2/common_layer/utils.py
+++ r2d2/common_layer/utils.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
+'''
+Utility functions for the common_layer
+'''
 
+def nearest(desired, options):
+    """
+    From the list, picks the option nearest to the number or date
+    
+    desired:  the number or date you'd like to be close to
+    options:  the list of options from which to pick the nearest option
+    
+    from:  http://stackoverflow.com/questions/32237862/find-the-closest-date-to-a-given-date
+    """
+    return min(options, key=lambda x: abs(x - desired))
 
 def map_id(account, value):
     """ ensure ID's are unique among different sources """
