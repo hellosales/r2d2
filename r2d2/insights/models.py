7bcb3615c43c9f8197519c427aca49463e33af1c
Matt Laszuk <gen@freshblend.com>
2017-01-03 17:34:15 -0500

    Modified architecture for Insights generation: - InsightModel is the metadata and logic for each individual type of Insight.  This subsumes insight generation functionality from the BaseGenerator classes - InsightDispatcher is the BaseGenerator subclass listening for the data_fetched signal.  This class is aware of all InsightModel and uses those, the Insight history, and the parameters to trigger to determine which InsightModel to ask to create an Insight
    This code is now forex aware and has been tested to convert to USD.
    
    17 new InsightModels were developed.  The three original Insights remain for testing purposes only.

--- r2d2/insights/models.py
+++ r2d2/insights/models.py
@@ -5,9 +5,7 @@ from django.db import models
 from django.db.models.signals import post_save
 
 from r2d2.accounts.models import Account
-from r2d2.insights.generators import AverageProductsPerTransactions
-from r2d2.insights.generators import AverageTransactionsPerWeek
-from r2d2.insights.generators import DataImportedInsight
+from r2d2.insights.generators import InsightDispatcher
 from r2d2.insights.signals import data_fetched
 from r2d2.insights.signals import insight_post_save
 
@@ -32,14 +30,51 @@ ALLOWED_CONTENT_TYPES = set([
     'text/csv',
 ]) | IMAGE_CONTENT_TYPES
 
-
+    
 class Insight(models.Model):
-    """ model to store insights """
+    """ 
+    model to store served insights 
+    """
     user = models.ForeignKey(Account, limit_choices_to={"approval_status": Account.APPROVED, 'is_active': True})
     created = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     generator_class = models.CharField(max_length=100, editable=False)
+    insight_model_id = models.IntegerField()
+    was_helpful = models.NullBooleanField() # whether the user voted helpful or unhelpful
 
+class Product(models.Model):
+    """
+    A product
+    """
+    insight = models.ForeignKey(Insight)
+    sku = models.CharField(max_length=500)
+    name = models.CharField(max_length=500)
+    
+class Channel(models.Model):
+    """
+    A DataProvider encapsulation that allows us to trap source info.
+    TODO:  remove this if the different data providers are managed more flexibly
+        as data via the DB instead of as separate apps in Django
+    """
+    insight = models.ForeignKey(Insight)
+    official_channel_name = models.CharField(max_length=500)
+    data_importer_class = models.CharField(max_length=500)
+    
+class InsightHistorySummary(models.Model):
+    """
+    Unmanaged model for the insights_insight_history_summary view that summarizes
+    how many of each InsightModel have been served, and what the most recent date
+    is for each
+    """
+    id = models.IntegerField(primary_key=True) # Placeholder id only, not meaningful
+    user = models.ForeignKey(Account, on_delete=models.DO_NOTHING)
+    insight_model_id = models.IntegerField()
+    count_insights = models.IntegerField()
+    most_recent = models.DateTimeField()
+    
+    class Meta:
+        managed = False
+        db_table = 'insights_insight_history_summary'
 
 def validate_file_extension(value):
     """ The file formats accepted should include excel files, csv files, word docs, .txt files, and .jpg files. """
@@ -76,6 +111,4 @@ class InsightAttachment(models.Model):
 post_save.connect(insight_post_save, sender=Insight)
 
 # IMPORTANT! order of connect will be the order of insights
-data_fetched.connect(DataImportedInsight.handle_data_fetched)
-data_fetched.connect(AverageTransactionsPerWeek.handle_data_fetched)
-data_fetched.connect(AverageProductsPerTransactions.handle_data_fetched)
+data_fetched.connect(InsightDispatcher.handle_data_fetched)
