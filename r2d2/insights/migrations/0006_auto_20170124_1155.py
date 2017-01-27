76773225b42aa1055bb9e174383b572c30a168b3
Matt Laszuk <gen@freshblend.com>
2017-01-27 16:49:02 -0500

    New migrations for Insight, CommonTransaction, and Channel

---
+++ r2d2/insights/migrations/0006_auto_20170124_1155.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('insights', '0005_auto_20170123_1336'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='channel',
+            old_name='data_importer_id',
+            new_name='data_provider_id',
+        ),
+        migrations.RenameField(
+            model_name='channel',
+            old_name='data_importer_name',
+            new_name='data_provider_name',
+        ),
+        migrations.RenameField(
+            model_name='insight',
+            old_name='data_importer_id',
+            new_name='data_provider_id',
+        ),
+        migrations.RenameField(
+            model_name='insight',
+            old_name='data_importer_name',
+            new_name='data_provider_name',
+        ),
+        migrations.RemoveField(
+            model_name='channel',
+            name='name',
+        ),
+    ]
