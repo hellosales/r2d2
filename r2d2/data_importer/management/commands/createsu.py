6bd24c5db9bf4c8f8980db4eeba1026228fc5fa1
Deno Vichas <deno@fullstacksystems.com>
2017-04-04 19:20:26 -0700

    config - create su

--- r2d2/data_importer/management/commands/createsu.py
+++ r2d2/data_importer/management/commands/createsu.py
@@ -1,11 +1,14 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+        admin_email = 'admin@hello-sales.com'
+        admin_password = '1q2w3e4r'
+
+        if not User.objects.filter(email=admin_email).exists():
+            User.objects.create_superuser(admin_email, admin_password)
