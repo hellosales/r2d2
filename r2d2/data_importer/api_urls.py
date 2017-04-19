1335f1b9bb73e5e028e68fe37e8b0c8533dbd595
Matt Laszuk <Matt.laszuk@gmail.com>
2017-04-19 16:43:18 -0400

    Added API endpoints for commands:  clearsessions, schedule_fetching_transactions, square_refresh_tokens, and update_rates.  Created and IsSuperUser permission class that all these commands require.

--- r2d2/data_importer/api_urls.py
+++ r2d2/data_importer/api_urls.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-""" etsy API urls """
+""" data_importer API urls """
 from django.conf.urls import patterns
 from django.conf.urls import url
 
 from r2d2.data_importer.api import DataImporterAccountsAPI
 from r2d2.data_importer.api import DataImporterGenerateOauthUrl
 from r2d2.data_importer.api import SuggestionCreateAPI
+from r2d2.data_importer.api import DataImporterRunFetchingData
 
 
 urlpatterns = patterns(
@@ -13,4 +14,5 @@ urlpatterns = patterns(
     url(r'^accounts$', DataImporterAccountsAPI.as_view(), name='data-importer-accounts'),
     url(r'^generate-oauth-url$', DataImporterGenerateOauthUrl.as_view(), name='data-importer-generate-oauth-url'),
     url(r'^source-sugesstions$', SuggestionCreateAPI.as_view(), name='source-suggestions-api'),
+    url(r'^run-fetching-data$', DataImporterRunFetchingData.as_view(), name='data-importer-run-fetching-data'),
 )
--- r2d2/squareup_api/api_urls.py
+++ r2d2/squareup_api/api_urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import url
 
 from r2d2.squareup_api.api import SquareupAccountAPI
 from r2d2.squareup_api.api import SquareupAccountListAPI
+from r2d2.squareup_api.api import SquareupRefreshTokensAPI
 
 
 urlpatterns = patterns(
     '',
     url(r'^accounts$', SquareupAccountListAPI.as_view(), name='squareup-accounts'),
     url(r'^accounts/(?P<pk>[\d]+)$', SquareupAccountAPI.as_view(), name='squareup-accounts'),
+    url(r'^squareup-refresh-tokens$', SquareupRefreshTokensAPI.as_view(), name='squareup-refresh-tokens-api'),
 )
